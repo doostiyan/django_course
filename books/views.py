95ef1a6632307b904eb149240c27fc172dd804c2
Amir Hossein Doostiyan <amirh9476@gmail.com>
2023-10-19 20:04:17 +0330

    j3 models

---
+++ books/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+from books.models import Book
+
+
+def books_list(request):
+    books = Book.objects.all()
+
+    return render(request, "books/list.html", {"books": books})
+# Create your views here.
--- project/urls.py
+++ project/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for books project.
+URL configuration for project project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
@@ -19,6 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("books/", include("management.urls"))
-
+    path("books/", include("books.urls"))
 ]
