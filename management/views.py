0f3339d301ba0c9b74702ad2f2a58f3169a77e4c
Amir Hossein Doostiyan <amirh9476@gmail.com>
2023-10-19 14:31:49 +0330

    first commit

--- management/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def hello(request):
-    return render(request, template_name="hello.html",context={
-        "user": "ehsan",
-        "is_valid": False
-    })
