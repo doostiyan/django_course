665e1084d19f83cb7d8976fb00fc23265fb23ddb
Amir Hossein Doostiyan <amirh9476@gmail.com>
2023-10-20 16:12:32 +0330

    j4 one to many

--- books/admin.py
+++ books/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from books.models import Book
+from books.models import Book,Auther
 
 admin.site.register(Book)
+admin.site.register(Auther)
+
 # Register your models here.
--- books/views.py
+++ books/views.py
@@ -8,3 +8,8 @@ def books_list(request):
 
     return render(request, "books/list.html", {"books": books})
 # Create your views here.
+
+def detail(request, book_id):
+    book = Book.objects.get(pk=book_id)
+
+    return render(request, template_name="books/detail.html", context={"book": book})
