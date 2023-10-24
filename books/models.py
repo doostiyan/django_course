58c6f91102e1afcee2fbc0d9d6914c6066d5bb41
Amir Hossein Doostiyan <amirh9476@gmail.com>
2023-10-24 17:52:30 +0330

    j5 cover field

--- books/models.py
+++ books/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Auther(models.Model):
     name = models.CharField(max_length=200)
     bio = models.TextField()
@@ -7,11 +8,14 @@ class Auther(models.Model):
     def __str__(self):
         return self.name
 
+
 class Book(models.Model):
     title = models.CharField(max_length=200)
-    author = models.ForeignKey(Auther, on_delete=models.CASCADE, null=True, blank=True)
+    author = models.ForeignKey(Auther, on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=9, decimal_places=0)
     description = models.TextField()
     publication_date = models.DateField()
+    cover = models.ImageField(upload_to="media/books/")
 
     def __str__(self):
         return self.title
--- books/views.py
+++ books/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 
 from books.models import Book
 
@@ -7,9 +7,18 @@ def books_list(request):
     books = Book.objects.all()
 
     return render(request, "books/list.html", {"books": books})
+
+
 # Create your views here.
 
 def detail(request, book_id):
     book = Book.objects.get(pk=book_id)
 
     return render(request, template_name="books/detail.html", context={"book": book})
+
+
+def delete(request, book_id):
+    book = get_object_or_404(Book, pk=book_id),
+    Book.objects.get(pk=book_id).delete()
+
+    return redirect("books:list")
